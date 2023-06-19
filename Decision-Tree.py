d0d1fee660ab04ed1411d5632c6e88923b613398
Masoud Vaez <57384861+vaezmasoud@users.noreply.github.com>
2023-06-19 14:03:04 +0330

    Update Decision-Tree.py

--- Decision-Tree.py
+++ Decision-Tree.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, auc, roc_curve
 
-data = pd.read_csv('/content/heart_cleveland_upload.csv')
+data = pd.read_csv('https://github.com/vaezmasoud/Heart-Disease/blob/main/heart_cleveland.csv')
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 
@@ -18,14 +18,28 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 
 y_pred_proba = classifier.predict_proba(X_test)[:, 1]
-auc = roc_auc_score(y_test, y_pred_proba)
-print(f"AUC: {auc}")
+fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
+auc_val = auc(fpr, tpr)
+print(f"AUC: {auc_val}")
 
 new_data = [[60, 1, 0, 130, 233, 1, 2, 150, 0, 2.3, 1, 0, 3]]
 # [[63, 1, 3, 145, 233, 1, 0, 150, 0, 2.3, 0, 0, 1]]
 prediction = classifier.predict(new_data)
 print(f"Prediction: {prediction}")
+
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % auc_val)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
 #------------------------------------------------------
 # Algorithm: Decision Tree
-# Accuracy: 0.7166666666666667
-# AUC: 0.7522321428571428
+# Accuracy: 0.7333333333333333
+# AUC: 0.734375
